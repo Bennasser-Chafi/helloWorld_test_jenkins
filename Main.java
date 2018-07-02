f244209afe6adf42d6dcd80190783c02333d70b2
BENNASSAR <chafi.bennassar@capgemini.com>
2018-07-02 10:35:11 +0100

    My first commit

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		for(int i =1; i<=10; i++){
-			System.out.print("Hello world this project to test jenkins with Sonar ... " + i);
+			System.out.println("Hello world this project to test jenkins with Sonar ... " + i);
 		}
 
 	}
